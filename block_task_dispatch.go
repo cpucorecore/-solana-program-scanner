d5e5afb87624ead1886074c34f34efb515483b0f
cpucorecore <cpucorecore@gmail.com>
2024-10-14 18:07:21 +0800

    first commit

---
+++ block_task_dispatch.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"sync"
+
+	"github.com/blocto/solana-go-sdk/rpc"
+)
+
+type BlockTaskDispatch struct {
+	cli rpc.RpcClient
+}
+
+func NewBlockTaskDispatch() *BlockTaskDispatch {
+	cli := rpc.NewRpcClient(conf.Solana.RpcEndpoint)
+	return &BlockTaskDispatch{cli: cli}
+}
+
+func (btd *BlockTaskDispatch) keepDispatchTaskMock(wg *sync.WaitGroup, startSlot uint64, count uint64, taskCh chan uint64) {
+	defer wg.Done()
+
+	start := startSlot
+	end := startSlot + count
+	for start < end {
+		taskCh <- start
+		start += 1
+	}
+
+	close(taskCh)
+}
